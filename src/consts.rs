cd6d76c2d3cc8a7edf910a90ef73c2e7e47eb3f0
0elek <87550550+0elek@users.noreply.github.com>
2023-11-17 03:50:39 +0100

    add ducmentation

--- src/consts.rs
+++ src/consts.rs
@@ -1,77 +1,60 @@
+/// The period of the lunar orbit in days.
 pub const ORBIT_PERIOD: f64 = 29.53058770576;
+
+/// The offset for the lunar orbit calculations.
 pub const ORBIT_OFFSET: f64 = 2451550.26;
 
+/// The period of the lunar distance in days.
 pub const DISTANCE_PERIOD: f64 = 27.55454988;
+
+/// The offset for the lunar distance calculations.
 pub const DISTANCE_OFFSET: f64 = 2451562.2;
 
+/// The base value for calculating lunation.
 pub const LUNATION_BASE: f64 = 2423436.6115277777;
 
+/// The mean radius of the Earth in kilometers.
 pub const EARTH_RADIUS_KM: f64 = 6371.0084;
 
 
+/// Represents a lunar phase with name, emoji and start and end fractions.
+#[derive(Debug, Clone, Copy)]
 pub struct Phase {
-    pub emoji: &'static str,
+    /// Name of the lunar phase.
     pub name: &'static str,
+    /// Emoji representing the lunar phase.
+    pub emoji: &'static str,
+    /// Start fraction of the lunar phase.
     pub start: f64,
+    /// End fraction of the lunar phase.
     pub end: f64,
 }
 pub const PHASES: [Phase; 8] = [
-    Phase {
-        emoji: "🌑",
-        name: "New Moon",
-        start: 0.0,
-        end: 0.02,
-    },
-    Phase {
-        emoji: "🌒",
-        name: "Waxing Crescent",
-        start: 0.02,
-        end: 0.22,
-    }, 
-    Phase {
-        emoji: "🌓",
-        name: "First Quarter",
-        start: 0.22,
-        end: 0.27,
-    },
-    Phase {
-        emoji: "🌔",
-        name: "Waxing Gibbous",
-        start: 0.27,
-        end: 0.47,
-    },
-    Phase {
-        emoji: "🌕",
-        name: "Full Moon",
-        start: 0.47,
-        end: 0.52,
-    },
-    Phase {
-        emoji: "🌖",
-        name: "Waning Gibbous",
-        start: 0.52,
-        end: 0.72,
-    },
-    Phase {
-        emoji: "🌗",
-        name: "Last Quarter",
-        start: 0.72,
-        end: 0.77,
-    },
-    Phase {
-        emoji: "🌘",
-        name: "Waning Crescent",
-        start: 0.77,
-        end: 1.0,
-    },
+    Phase { emoji: "🌑", name: "New Moon", start: 0.0, end: 0.02 },
+    Phase { emoji: "🌒", name: "Waxing Crescent", start: 0.02, end: 0.22 }, 
+    Phase { emoji: "🌓", name: "First Quarter", start: 0.22, end: 0.27 },
+    Phase { emoji: "🌔", name: "Waxing Gibbous", start: 0.27, end: 0.47 },
+    Phase { emoji: "🌕", name: "Full Moon", start: 0.47, end: 0.52 },
+    Phase { emoji: "🌖", name: "Waning Gibbous", start: 0.52, end: 0.72 },
+    Phase { emoji: "🌗", name: "Last Quarter", start: 0.72, end: 0.77 },
+    Phase { emoji: "🌘", name: "Waning Crescent", start: 0.77, end: 1.0 },
 ];
 
+/// Represents information about the moon, including its julian date, phase,
+/// age, illumination, distance, and lunation.
 #[derive(Debug, Clone, Copy)]
 pub struct Moon {
+    /// A continuous count of days and fractions since noon Universal Time on January 1, 4713 BC 
     pub julian_date: f64,
+    /// Phase of the moon.
     pub phase: f64,
-    pub age: f64, 
-    pub illumination: f64, // 0 to 1 where is 1 is full moon
-    pub distance: f64, // in earth radii
-    pub lunation: u16, // brown l
+    /// Age of the moon.
+    pub age: f64,
+    /// Illumination of the moon (0 to 1 where 1 is a full moon).
+    pub illumination: f64,
+    /// Distance of the moon in earth radii.
+    pub distance: f64,
+    /// Lunation number.
+    pub lunation: u16,
 }
+
--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,33 @@ use std::time::SystemTime;
 
 mod consts;
 pub use consts::*;
-
+/// Calculates the Julian date of the moon based on the provided `SystemTime`.
 fn julian_date(time: SystemTime) -> f64 {
     let unix_time: f64 = match time.duration_since(SystemTime::UNIX_EPOCH) {
         Ok(duration) => duration.as_secs_f64(),
-        Err(earlier ) => -1. * earlier.duration().as_secs_f64(),
+        Err(earlier) => -1. * earlier.duration().as_secs_f64(),
     };
     unix_time / 86400. + 2440587.5
 }
+/// Calculates the illumination of the moon based on the given phase.
 fn illumination(phase: f64) -> f64 {
     0.5 * (1.0 - (TAU * phase).cos())
 }
+
+/// Calculates the lunation (lunar month count) based on the provided Julian date.
 fn lunation(julian_date: f64) -> u16 {
     (1. + (julian_date - LUNATION_BASE) / ORBIT_PERIOD).floor() as u16
 }
 
+/// Calculates the phase of the moon based on the provided Julian date.
 fn phase(julian_date: f64) -> f64 {
     ((julian_date - ORBIT_OFFSET) / ORBIT_PERIOD).fract()
 }
+/// Calculates the distance phase of the moon based on the provided Julian date.
 fn distance_phase(julian_date: f64) -> f64 {
     ((julian_date - DISTANCE_OFFSET) / DISTANCE_PERIOD).fract()
 }
-fn distance(phase:f64, julian_date: f64) -> f64 {
-
+fn distance(phase: f64, julian_date: f64) -> f64 {
     let distance_p = distance_phase(julian_date);
     let distance_p_tau: f64 = TAU * distance_p;
     let p_tau: f64 = 2.0 * TAU * phase;
@@ -41,7 +45,7 @@ impl Moon {
         let illumination: f64 = illumination(phase);
         let lunation: u16 = lunation(julian_date);
         let distance: f64 = distance(phase, julian_date);
-        
+
         Moon {
             julian_date,
             age,
@@ -51,16 +55,22 @@ impl Moon {
             lunation,
         }
     }
+
+    /// Returns the distance of the moon in kilometers.
     pub fn distance_km(&self) -> f64 {
         self.distance * EARTH_RADIUS_KM
     }
-    pub fn is_waning(&self)-> bool{
+
+    /// Checks if the moon is in the waning phase.
+    pub fn is_waning(&self) -> bool {
         self.age < 0.5
     }
-    pub fn is_waxing(&self)-> bool{
+    /// Checks if the moon is in the waxing phase.
+    pub fn is_waxing(&self) -> bool {
         self.age > 0.5
     }
-    pub fn phase_name(&self, ) -> &'static str {
+    /// Returns the name of the moon phase.
+    pub fn phase_name(&self) -> &'static str {
         for phase in PHASES.iter() {
             if self.phase >= phase.start && self.phase < phase.end {
                 return phase.name;
@@ -68,7 +78,9 @@ impl Moon {
         }
         "Unknown"
     }
-    pub fn phase_emoji(&self, ) -> &'static str {
+
+    /// Returns the emoji representation of the moon phase.
+    pub fn phase_emoji(&self) -> &'static str {
         for phase in PHASES.iter() {
             if self.phase >= phase.start && self.phase < phase.end {
                 return phase.emoji;
@@ -76,5 +88,4 @@ impl Moon {
         }
         "Unknown"
     }
-    
 }
